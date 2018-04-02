5169c3897dc25b400ce72fabc27fde144a70d3ef
Mathis Wiehl <mathis.wiehl@sinnerschrader.com>
2018-04-02 15:15:21 +0200

    chore: adjust test matcher to only match src tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json']
 };
